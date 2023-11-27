6d52a5023f9fb3e5d6f13d8598f36db239a3b380
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-27 15:48:39 +0000

    Add h3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to the Paws and Claws Emporium!</h1>
       <h2>Now with more minerals</h2>
+      <h2>And more...!</h2>
     </main>
   );
 }
