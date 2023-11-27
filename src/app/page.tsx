5a73c3e0fa1cf62e2710dd35a6cb23548993933b
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-27 14:27:26 +0000

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the Emporium!</h1>
+      <h1>Welcome to the Paws and Claws Emporium!</h1>
     </main>
   );
 }
